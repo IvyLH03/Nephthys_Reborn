cfae6130a0d56fecc7d53e9ff78acefcc10f4847
IvyLH03 <hanzhangzhu@outlook.com>
2023-02-09 23:50:09 -0600

    Create MessageQueue.java

--- src/main/java/structure/MessageQueue.java
+++ src/main/java/structure/MessageQueue.java
@@ -1,4 +1,10 @@
 package structure;
 
-public class MessageQueue {
+import structure.task.Task;
+
+/**
+ * A message queue data structure that can be used to store tasks.
+ */
+public class MessageQueue <T extends Task> {
+
 }
